7c4937e1cbbcfa08f84baf1d6d54eba855a0c403
wojwarych <woj.warych@gmail.com>
2024-06-12 14:43:59 +0200

    style(app): Fixes after linter

--- mini_sedric/main.py
+++ mini_sedric/main.py
@@ -1,3 +1,5 @@
+"""Main entrypoint for MiniSedric app"""
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,4 +7,5 @@ app = FastAPI()
 
 @app.get("/")
 async def hello_world():
+    """Basic hello world entrypoint for app"""
     return "Hello, MiniSedric!"
