796978af52bafecd953a96a4f821a74a2eefc239
wojwarych <woj.warych@gmail.com>
2024-06-15 15:03:27 +0200

    chore(linters): Extra changes in linters

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -3,8 +3,17 @@ import subprocess
 
 def format():
     subprocess.run(["black", "."])
-    subprocess.run(["autoflake", "-r", "--remove-all-unused-imports", "mini_sedric"])
     subprocess.run(["isort", "."])
     subprocess.run(["flake8", "mini_sedric/", "tests/"])
+    subprocess.run(
+        [
+            "autoflake",
+            "-r",
+            "--remove-all-unused-imports",
+            "--ignore-init-module-imports",
+            "mini_sedric",
+            "tests",
+        ]
+    )
     subprocess.run(["pylint", "mini_sedric/", "tests/"])
     subprocess.run(["mypy", "mini_sedric/", "tests/"])
