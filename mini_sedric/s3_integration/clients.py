079f890a2bdde20d0beac3e35b1be1daf968550c
wojwarych <woj.warych@gmail.com>
2024-06-16 17:36:00 +0200

    feat(s3): Basic S3 interface with real AWS service

--- mini_sedric/s3_integration/clients.py
+++ mini_sedric/s3_integration/clients.py
@@ -1,5 +1,10 @@
 """Module for implementations of S3Interface ABC class"""
 
+import boto3
+import botocore
+
+from mini_sedric.config import settings
+
 from .interface import S3Interface
 
 
@@ -14,3 +19,32 @@ class LocalS3Interface(S3Interface):
 
     def close(self) -> None:
         return
+
+
+class S3AWSInterface(S3Interface):
+    """Class for communicating with S3 interface of AWS"""
+
+    def __init__(self) -> None:
+        self.s3_client = boto3.client(
+            "s3",
+            endpoint_url=settings.aws_endpoint,
+            aws_access_key_id=settings.aws_access_key_id,
+            aws_secret_access_key=settings.aws_secret_access_key,
+            region_name=settings.region_name,
+        )
+
+    def check(self, uri: str) -> bool:
+        try:
+            sanitized_uri = uri.lstrip("s3://").rsplit("/")
+            self.s3_client.head_object(Bucket=sanitized_uri[0], Key=sanitized_uri[1])
+        except botocore.exceptions.ClientError as e:
+            if e.response["Error"]["Code"] == "404":
+                print("Object not found!")
+            else:
+                print("Unexpected error!")
+                print(e.response)
+            return False
+        return True
+
+    def close(self) -> None:
+        self.s3_client.close()
