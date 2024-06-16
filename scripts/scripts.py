dc6465a5ba77ac8f7f380b1401ae29a69bf7e50e
wojwarych <woj.warych@gmail.com>
2024-06-16 19:49:56 +0200

    chore(linters): Added check instead of just format

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -9,6 +9,26 @@ def format():
         [
             "autoflake",
             "-r",
+            "--cd",
+            "--remove-all-unused-imports",
+            "--ignore-init-module-imports",
+            "mini_sedric",
+            "tests",
+        ]
+    )
+    subprocess.run(["pylint", "mini_sedric/", "tests/"])
+    subprocess.run(["mypy", "mini_sedric/", "tests/"])
+
+
+def check():
+    subprocess.run(["black", "--check", "."])
+    subprocess.run(["isort", "--check", "."])
+    subprocess.run(["flake8", "mini_sedric/", "tests/"])
+    subprocess.run(
+        [
+            "autoflake",
+            "-r",
+            "-c",
             "--remove-all-unused-imports",
             "--ignore-init-module-imports",
             "mini_sedric",
