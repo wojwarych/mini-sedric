ead0a3f976b701ca0af08f226f8c5a03d6fc1d63
wojwarych <woj.warych@gmail.com>
2024-06-16 23:06:23 +0200

    docs: Docstrings updates

--- mini_sedric/s3_integration/connection.py
+++ mini_sedric/s3_integration/connection.py
@@ -9,7 +9,11 @@ from .interface import S3Interface
 
 
 async def connect_to_s3() -> AsyncIterator[S3Interface]:
-    """Creates connection dependency injection of S3Interface"""
+    """Creates connection dependency injection of S3Interface
+
+    Yields:
+        AsyncIterator[S3Interface]: Instance of the interface
+    """
     s3_conn = (
         LocalS3Interface()
         if settings.env_for_dynaconf == "testing"
