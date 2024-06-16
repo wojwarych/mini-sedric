7f2b94fe13ab10272e71aa2daeac9ec1d2ef9ffd
wojwarych <woj.warych@gmail.com>
2024-06-16 23:06:23 +0200

    docs: Docstrings updates

--- mini_sedric/models/models.py
+++ mini_sedric/models/models.py
@@ -4,7 +4,12 @@ from pydantic import BaseModel, Field
 
 
 class InteractionInput(BaseModel):
-    """Model for request body of /insights endpoint"""
+    """Model for request body of /insights endpoint
+
+    Attributes:
+        interaction_url (str): URI in S3 format to the desired audio MP3 file
+        trackers (list[str]): list of searched words in the transcription
+    """
 
     interaction_url: str = Field(pattern=r"^s3:\S+(.mp3)$")
     trackers: list[str]
