7c3edb90e851f689bbed8f4c3bd77c0be188d7dc
wojwarych <woj.warych@gmail.com>
2024-06-18 12:21:46 +0200

    feat(transcribe): Added example mp3 for dev

--- init-aws.sh
+++ init-aws.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 awslocal s3 mb s3://test-bucket
-awslocal s3 cp /var/lib/localstack/testAudio.mp3 s3://test-bucket/testAudio.mp3
+awslocal s3 cp /var/lib/localstack/emma.mp3 s3://test-bucket/emma.mp3
